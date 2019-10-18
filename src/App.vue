a910243a90c2bae287b554d2b2458b6c10ee163c
Alexander Fridriksson <post@alexanderfridriksson.com>
2019-10-18 11:23:37 +0100

    Turned login into a component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,20 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field 
-            label="Username" 
-            prepend-icon="mdi-account-circle"
-            />
-          <v-text-field 
-            :type="showPassword ? 'text' : 'password'"
-            label="Password" 
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-            />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <login></login>
   </v-app>
 </template>
 
 
 <script>
+import login from './components/login';
+
 export default {
   name: 'App',
+  components: {
+    login,
+  },
   data: () => ({
-    showPassword: false
+    //
   }),
 };
 </script>
